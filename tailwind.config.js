ff35d0666ac7164b423aec67f8bdd1d43707c3db
Ilko Ivanov <ilko.iv@gmail.com>
2024-10-27 14:38:07 +0000

    Add Exo 2 font

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        sans: ['"Exo 2"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         customGray: {
